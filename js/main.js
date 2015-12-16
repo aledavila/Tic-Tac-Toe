8cf85c9fdf8c7c9b9c661710cced4075b244c9ea
Alessandra Davila <aledavila>
2015-12-16 12:35:23 -0600

    starts project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+//alert are you ready to play
+// type in names to set as player 1 and 2
+
+//  possible wins =
+// top 1, top 2, top 3
+// middle 1, middle 2, middle 3
+// bottom 1, bottom 2, bottom 3
+// top 1, middle 1, bottom 1
+// top 2, middle 2, bottom 2
+// top 3, middle 3, bottom 3
+// top 1, middle 2, bottom 3
+// top 3, middle 2, bottom 1
+
+//if player 1 when choosing a space mark as x automatically
+//if player 2 when choosing a space mark as o automatically
+//if space is clicked mark as either x or y depending on who's turn it is
+
+//alert players of who the winner is:
+//player 1
+//player 2
+//draw = "cat's game"
+
+//on click of new game:
+//clear the board
+//and alert player of new game
+
+//on reset:
+//reset game and prompt players to enter name
+
+//lock choice in
+//if player chooses an occupied spot alert player space is taken
+
+//on hover change box color
