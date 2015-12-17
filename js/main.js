4e8c32e643f5a22da0edae07ab2ac5b20ad9e42c
Alessandra Davila <aledavila>
2015-12-16 19:45:12 -0600

    begins loops to evalute the board

--- js/main.js
+++ js/main.js
@@ -1,86 +1,83 @@
 $(function() {
-//alert are you ready to play
-  alert("Are you ready to play Tic Tac Toe?");
+    //alert are you ready to play
+    alert("Are you ready to play Tic Tac Toe?");
+
+    var move = 1;
 
 
-var move = 1;
+    $(".box").click(function() {
+        if (move % 2 === 0) {
+            var id = $(this).attr('id');
+            $(this).append("<i class='fa fa-circle-o fa-5x'></i>");
+            //if player chooses an occupied becomes unclickable
+            $(this).css("pointer-events", "none");
+            move++;
+            console.log(id);
+        } else {
+            var id = $(this).attr('id');
+            $(this).append("<i class='fa fa-times fa-5x'></i>");
+            //if player chooses an occupied becomes unclickable
+            $(this).css("pointer-events", "none");
+            move++;
+            console.log(id);
+        }
+    });
 
-  $(".box").click (function() {
-    if(move % 2 === 0) {
-      $(this).append("<i class='fa fa-circle-o fa-5x'></i>");
-      $(this).css("pointer-events", "none");
-      move++;
-    } else {
-      $(this).append("<i class='fa fa-times fa-5x'></i>");
-      $(this).css("pointer-events", "none");
-      move++;
+    ///if 1 === 2 ===  3
+    //add value after each click
+
+
+    //will highlight who's turn it is
+    // if(move % 2 === 0) {
+    // } else if (move % 2 === 1) {
+
+
+    //  possible wins =
+    // top 1, top 2, top 3
+    // middle 1, middle 2, middle 3
+    // bottom 1, bottom 2, bottom 3
+    // top 1, middle 1, bottom 1
+    // top 2, middle 2, bottom 2
+    // top 3, middle 3, bottom 3
+    // top 1, middle 2, bottom 3
+    // // // top 3, middle 2, bottom 1
+    // if ((player1 === ) || (player1 === ) || (player1 === "s7", "s8", "s9") || (player1 === "s1", "s4", "s7") || (player1 === "s2", "s5", "s8") || (player1 === "s3", "s6", "s9") || (player1 === "s1", "s5", "s9") || (player1 === "s3", "s5", "s7")) {
+
+    // // }
+    // // else if If ((player2 === ) || (player2 === "s4", "s5", "s6") || (player2 === "s7", "s8", "s9") || (player2 === "s1", "s4", "s7") || (player2 === "s2", "s5", "s8") || (player2 === "s3", "s6", "s9") || (player2 === "s1", "s5", "s9") || (player2 === "s3", "s5", "s7")) {
+
+    // // }
+    //alert players of who the winner is:
+    //player 1
+    //player 2
+    function getWinner() {
+        if (winnerIsX()) {
+            alert("Winner is Player 1!");
+        }
+        else if (winnerIsO()) {
+            alert("Winner is Player 2!");
+        }
+        else {
+            alert("Cat's Game");
+        }
     }
-  });
 
-//will highlight who's turn it is
-// if(move % 2 === 0) {
-// } else if (move % 2 === 1) {
+    //on click of new game:
+    //clear the board
+    //and alert player of new game
+    $(".btn-primary").on("click", function() {
 
+    });
 
-//  possible wins =
-// top 1, top 2, top 3
-// middle 1, middle 2, middle 3
-// bottom 1, bottom 2, bottom 3
-// top 1, middle 1, bottom 1
-// top 2, middle 2, bottom 2
-// top 3, middle 3, bottom 3
-// top 1, middle 2, bottom 3
-// top 3, middle 2, bottom 1
+    //on reset:
+    //reset game and prompt players to enter name
+    $(".btn-danger").on("click", function() {
 
+    });
 
-//alert players of who the winner is:
-//player 1
-//player 2
-//draw = "cat's game"
-
-//on click of new game:
-//clear the board
-//and alert player of new game
-
-//on reset:
-//reset game and prompt players to enter name
-
-//lock choice in
-//if player chooses an occupied spot alert player space is taken
-
-//on hover change box color
+    //on hover change box color
 
 
 });
 
 
-
-// function getWinner() {
-//     if (isWinner('x')) {
-//       return 'x';
-//     }
-//     if (isWinner('o')) {
-//       return 'o';
-//     }
-//     return null;
-// }
-// function isWinner(player) {
-//     return winsRow(player) || winsColumn(player) || winsDiagonal(player);
-// }
-// function winsRow(player) {
-//     return allThree(player, cells('a'), cells('b'), cells('c')) ||
-//            allThree(player, cells('d'), cells('e'), cells('f')) ||
-//            allThree(player, cells('g'), cells('h'), cells('i'));
-// }
-// function winsColumn(player) {
-//     return allThree(player, cells('a'), cells('d'), cells('g')) ||
-//            allThree(player, cells('b'), cells('e'), cells('h')) ||
-//            allThree(player, cells('c'), cells('f'), cells('i'));
-// }
-// function winsDiagonal(player) {
-//     return allThree(player, cells('a'), cells('e'), cells('i')) ||
-//            allThree(player, cells('c'), cells('e'), cells('g'));
-// }
-// function allThree(player, cell_one, cell_two, cell_three) {
-//     return (cell_one === player) && (cell_two === player) && (cell_three === player);
-// }
