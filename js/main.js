1326c16d27c5c6ba949d96b00146854aa81ceceb
Alessandra Davila <aledavila>
2015-12-17 18:54:59 -0600

    adds game logic and mvp

--- js/main.js
+++ js/main.js
@@ -3,76 +3,147 @@ $(function() {
     alert("Are you ready to play Tic Tac Toe?");
 
     var move = 1;
+    var player1 = [];
+    var player2 = [];
+    var winner = null;
+
 
 
     $(".box").click(function() {
         if (move % 2 === 0) {
+              // playerMove('fa-circle-o', player2, 'player2', ".box");
             var id = $(this).attr('id');
+          //add value after each click
             $(this).append("<i class='fa fa-circle-o fa-5x'></i>");
-            //if player chooses an occupied becomes unclickable
+          //if player chooses an occupied becomes unclickable
             $(this).css("pointer-events", "none");
             move++;
-            console.log(id);
+            player2.push(id);
+            console.log(player2);
+            $(".player1").addClass('yellow');
+            $(".player2").removeClass('yellow');
+            row();
+            column();
+            diagonal();
+            checkTie();
         } else {
+              // playerMove('fa-times', player1, 'player1', ".box");
             var id = $(this).attr('id');
+          //add value after each click
             $(this).append("<i class='fa fa-times fa-5x'></i>");
-            //if player chooses an occupied becomes unclickable
+          //if player chooses an occupied becomes unclickable
             $(this).css("pointer-events", "none");
             move++;
-            console.log(id);
+            player1.push(id);
+            console.log(player1);
+            $(".player2").addClass('yellow');
+            $(".player1").removeClass('yellow');
+            row();
+            column();
+            diagonal();
+            checkTie();
         }
     });
 
-    ///if 1 === 2 ===  3
-    //add value after each click
+    // function playerMove(symbol, moveArray, playerClass, el) {
+    //   var offPlayerClass = (playerClass === 'player1') ? 'player2' : 'player1';
+    //   var id = $(this).attr('id');
+    //   console.log(id);
+    // //add value after each click
+    //   $(this).append("<i class='fa "+ symbol +" fa-5x'></i>");
+    //   console.log(id);
+    // //if player chooses an occupied becomes unclickable
+    //   $(this).css("pointer-events", "none");
+    //   move++;
+    //   moveArray.push(id);
+    //   console.log(moveArray);
+    //   $("."+offPlayerClass).addClass('yellow');
+    //   $("."+playerClass).removeClass('yellow');
+    //   row();
+    //   column();
+    //   diagonal;
+    // }
 
 
-    //will highlight who's turn it is
-    // if(move % 2 === 0) {
-    // } else if (move % 2 === 1) {
+  function row() {
+      if (player1.includes('top-one') && player1.includes('top-two') && player1.includes('top-three') ||
+          player1.includes('middle-one') && player1.includes('middle-two') && player1.includes('middle-three') ||
+          player1.includes('bottom-one') && player1.includes('bottom-two') && player1.includes('bottom-three')) {
+            checkWinner("player 1");
+            $(".box").css("pointer-events", "none");
+      } else if (player2.includes('top-one') && player2.includes('top-two') && player2.includes('top-three') ||
+                 player2.includes('middle-one') && player2.includes('middle-two') && player2.includes('middle-three') ||
+                 player2.includes('bottom-one') && player2.includes('bottom-two') && player2.includes('bottom-three')) {
+            checkWinner("player 2");
+            $(".box").css("pointer-events", "none");
+      }
+  }
 
+  function column() {
+      if (player1.includes('top-one') && player1.includes('middle-one') && player1.includes('bottom-one') ||
+          player1.includes('top-two') && player1.includes('middle-two') && player1.includes('bottom-two') ||
+          player1.includes('top-three') && player1.includes('middle-three') && player1.includes('bottom-three')) {
+            checkWinner("player 1");
+            $(".box").css("pointer-events", "none");
+      } else if (player2.includes('top-one') && player2.includes('middle-one') && player2.includes('bottom-one') ||
+                 player2.includes('top-two') && player2.includes('middle-two') && player2.includes('bottom-two') ||
+                 player2.includes('top-three') && player2.includes('middle-three') && player2.includes("bottom-three")) {
+            checkWinner("player 2");
+            $(".box").css("pointer-events", "none");
+      }
+  }
 
-    //  possible wins =
-    // top 1, top 2, top 3
-    // middle 1, middle 2, middle 3
-    // bottom 1, bottom 2, bottom 3
-    // top 1, middle 1, bottom 1
-    // top 2, middle 2, bottom 2
-    // top 3, middle 3, bottom 3
-    // top 1, middle 2, bottom 3
-    // // // top 3, middle 2, bottom 1
-    // if ((player1 === ) || (player1 === ) || (player1 === "s7", "s8", "s9") || (player1 === "s1", "s4", "s7") || (player1 === "s2", "s5", "s8") || (player1 === "s3", "s6", "s9") || (player1 === "s1", "s5", "s9") || (player1 === "s3", "s5", "s7")) {
-
-    // // }
-    // // else if If ((player2 === ) || (player2 === "s4", "s5", "s6") || (player2 === "s7", "s8", "s9") || (player2 === "s1", "s4", "s7") || (player2 === "s2", "s5", "s8") || (player2 === "s3", "s6", "s9") || (player2 === "s1", "s5", "s9") || (player2 === "s3", "s5", "s7")) {
-
-    // // }
-    //alert players of who the winner is:
-    //player 1
-    //player 2
-    function getWinner() {
-        if (winnerIsX()) {
-            alert("Winner is Player 1!");
-        }
-        else if (winnerIsO()) {
-            alert("Winner is Player 2!");
-        }
-        else {
-            alert("Cat's Game");
-        }
+  function diagonal() {
+    if(player1.includes('top-one') && player1.includes('middle-two') && player1.includes('bottom-three') ||
+       player1.includes('top-three') && player1.includes('middle-two') && player1.includes('bottom-one')) {
+          checkWinner("player 1");
+          $(".box").css("pointer-events", "none");
+    } else if (player2.includes('top-one') && player2.includes('middle-two') && player2.includes('bottom-three') ||
+               player2.includes('top-three') && player2.includes('middle-two') && player2.includes('bottom-one')) {
+          checkWinner("player 2");
+          $(".box").css("pointer-events", "none");
     }
+  }
+
+  function checkWinner(player) {
+    alert(player + "is the winner!");
+    winner = player;
+  }
+
+  function checkTie() {
+    var one = player1.length;
+    var two = player2.length;
+    if(one + two >= 9 && !winner) {
+      alert("tie");
+    }
+  }
+  // function draw() {
+  //   if(move >= 9 && )
+  // }
+
+  //   // function getWinner() {
+    //     if (winnerIsX()) {
+    //         alert("Winner is Player 1!");
+    //     }
+    //     else if (winnerIsO()) {
+    //         alert("Winner is Player 2!");
+    //     }
+    //     else {
+    //         alert("Cat's Game");
+    //     }
+    // }
 
     //on click of new game:
     //clear the board
     //and alert player of new game
-    $(".btn-primary").on("click", function() {
-
-    });
+    // $(".btn-primary").on("click", function() {
+    //     location.reload();
+    // });
 
     //on reset:
     //reset game and prompt players to enter name
     $(".btn-danger").on("click", function() {
-
+        location.reload();
     });
 
     //on hover change box color
